99143c6f43bac035e1c59739f4c610d3a03696f4
Matteo Omicini <matteoomicini.dev@gmail.com>
2025-02-18 17:58:30 +0100

    fix: useGetPost types

--- composables/useGetPosts.ts
+++ composables/useGetPosts.ts
@@ -19,7 +19,6 @@ export const useGetPosts = async (): Promise<Post[]> => {
             return []
         }
 
-        // Map the fetched data to your custom structure
         posts.value = data.value.map((post: PostDTO): Post => ({
             image: `https://picsum.photos/seed/${post.id}/200/300`,
             title: post.title,
@@ -30,14 +29,13 @@ export const useGetPosts = async (): Promise<Post[]> => {
                 avatar: 'https://randomuser.me/api/portraits/men/50.jpg',
             },
             id: post.id.toString(),
-            // Since jsonplaceholder doesn't have createdAt/updatedAt, use mock dates or remove
             createdAt: new Date().toLocaleString(), // mock date or add another date field
             updatedAt: new Date().toLocaleString(), // mock date or add another date field
         }))
 
-        return posts
+        return posts.value
     } catch (err) {
         console.error(err)
-        return {posts: []}
+        return []
     }
 }
